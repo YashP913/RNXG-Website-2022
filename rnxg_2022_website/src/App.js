91825074b8c6fbb75e6b2476eadddf131dbc4249
YashP913 <2021bcs024@sggs.ac.in>
2022-11-05 17:48:39 +0530

    firstc_commit

--- rnxg_2022_website/src/App.js
+++ rnxg_2022_website/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Footer from "./Components/Footer";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App"> <Footer /></div>;
 }
 
 export default App;
