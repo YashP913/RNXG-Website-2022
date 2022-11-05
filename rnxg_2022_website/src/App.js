eab9f452fcc0fe1e1827b3a3e0537540101c5303
YashP913 <2021bcs024@sggs.ac.in>
2022-11-05 17:50:59 +0530

    test_delition

--- rnxg_2022_website/src/App.js
+++ rnxg_2022_website/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Footer from "./Components/Footer";
+// import Footer from "./Components/Footer";
 
 function App() {
-  return <div className="App"> <Footer /></div>;
+  return <div className="App"> </div>;
 }
 
 export default App;
